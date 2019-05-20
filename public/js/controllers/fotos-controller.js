50de64219bc223f7896ee9d8cd3e65e26e24589e
Edilon Andrade <edilonandrade@gmail.com>
2019-05-20 18:25:05 -0300

    Mudança com a repetição por objetos diferentes.

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,4 +3,15 @@ angular.module('alurapic').controller('FotosController', function($scope) {
         titulo : "Leão",
         url : "http://www.fundosanimais.com/Minis/leoes.jpg"
     };
+
+    $scope.foto2 = {
+        titulo : "Leão 2",
+        url : "http://www.fundosanimais.com/Minis/leoes.jpg"
+    };
+
+    $scope.foto3 = {
+        titulo : "Leão 3",
+        url : "http://www.fundosanimais.com/Minis/leoes.jpg"
+    };
+
 });
